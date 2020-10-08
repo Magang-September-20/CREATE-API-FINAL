13ca0795b2ff056f9f105c1c182d01cf8bc0b28e
Jonathan purnama halim <46183646+nattnathan@users.noreply.github.com>
2020-10-08 21:27:17 +0700

    semangat herul

--- APIfinal/APIfinal/src/main/java/com/MII/APIfinal/ApIfinalApplication.java
+++ APIfinal/APIfinal/src/main/java/com/MII/APIfinal/ApIfinalApplication.java
@@ -8,6 +8,7 @@ public class ApIfinalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApIfinalApplication.class, args);
+                System.out.println("====== jalan gaes ======");
 	}
 
 }
