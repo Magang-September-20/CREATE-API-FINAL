e55f68fba6d34a07aa6f70ac8bdcef97dcbbec33
Jonathan purnama halim <46183646+nattnathan@users.noreply.github.com>
2020-10-13 14:13:43 +0700

    commit1

--- APIfinal/APIfinal/src/main/java/com/MII/APIfinal/repositories/UserRepository.java
+++ APIfinal/APIfinal/src/main/java/com/MII/APIfinal/repositories/UserRepository.java
@@ -6,12 +6,15 @@
 package com.MII.APIfinal.repositories;
 
 import com.MII.APIfinal.entities.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  *
  * @author cadiize
  */
 public interface UserRepository extends JpaRepository<User, Integer>{
-    
+    @Query(value = "SELECT * FROM user",nativeQuery = true)
+    public List<User> getAll();
 }
